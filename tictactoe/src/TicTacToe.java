d6aee2bdda1684ecc0778c0f1cfefec537d87ae1
thomasgardner47 <64431748+thomasgardner47@users.noreply.github.com>
2021-03-11 23:22:14 +0000

    Added game board to tictactoe.java file
    Gameboard is now setup
    
    char array now contained gameboard variable which is being passed through in printGameBoard method.

--- tictactoe/src/TicTacToe.java
+++ tictactoe/src/TicTacToe.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
 
         //2D array of chars
 
-        char[] [] gameBoard = {{' ', '|'}, {}, {}, {}};
+        char[] [] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
 
+        printGameBoard(gameBoard);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9): ");
+        int pos = scan.nextInt();
+
+        System.out.println(pos);
+    }
+
+    public static void printGameBoard(char [] [] gameBoard){
+        for (char [] row : gameBoard) {
+            for (char c : row) {
+                System.out.println(c);
+            }
+            System.out.println();
+        }
     }
 }
