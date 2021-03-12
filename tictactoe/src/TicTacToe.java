1b802f67acae38404ba7eccf016c71056f8415bc
thomasgardner47 <64431748+thomasgardner47@users.noreply.github.com>
2021-03-12 13:21:22 +0000

    added more logic to gameBoard
    Added function which allows system to choose which symbol to play in game, either X or O.
    
    Moved switch positioning function into it's own method

--- tictactoe/src/TicTacToe.java
+++ tictactoe/src/TicTacToe.java
@@ -27,36 +27,8 @@ public class TicTacToe {
 
         System.out.println(pos);
 
-        switch(pos) {
-            case 1:
-                gameBoard[0][0] = 'X';
-                break;
-            case 2:
-                gameBoard[0][2] = 'X';
-                break;
-            case 3:
-                gameBoard[0][4] = 'X';
-                break;
-            case 4:
-                gameBoard[2][0] = 'X';
-                break;
-            case 5:
-                gameBoard[2][2] = 'X';
-                break;
-            case 6:
-                gameBoard[2][4] = 'X';
-                break;
-            case 7:
-                gameBoard[4][0] = 'X';
-                break;
-            case 8:
-                gameBoard[4][2] = 'X';
-                break;
-            case 9:
-                gameBoard[4][4] = 'X';
-                break;
+        placePiece(gameBoard, pos, "player");
 
-        }
         printGameBoard(gameBoard);
     }
 
@@ -66,6 +38,49 @@ public class TicTacToe {
                 System.out.println(c);
             }
             System.out.println();
+        }
+    }
+
+    public static void placePiece(char[][] gameBoard, int pos /* pass in a position */ , String user /*define a user*/) {
+
+        char symbol = ' ';
+
+
+        if(user.equals("player")) {
+            symbol = 'X';
+        } else if (user.equals("cpu")) {
+            symbol = '0';
+        } // this way we can switch between X and O depending on what player we give the system
+
+        switch(pos) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+
         }
     }
 }
